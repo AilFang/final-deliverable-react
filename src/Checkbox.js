69958f74251dad76754bd6ffce45c959524faa25
Aileen Fang <aileenf915@gmail.com>
2024-03-15 14:56:25 -0400

    value update

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,19 +1,23 @@
 import "./Map.css";
 import React, { useState } from "react";
 
-
 // pull in rooms & setRooms from MainFloor props
+// sami -> aileen
+// find the key of the id in rooms
+// set the value of the id's isChecked to the opposed value
+// let updatedRooms = logic
+// setRooms(updatedRooms);
+
 function Checkbox({ label, onChange }) {
   const [isChecked, setIsChecked] = useState(false);
 
   const checkHandler = () => {
-    setIsChecked(!isChecked);
-
-    // sami -> aileen
-    // find the key of the id in rooms
-    // set the value of the id's isChecked to the opposed value
-    // let updatedRooms = logic
-    // setRooms(updatedRooms);
+    const newValue = !isChecked;
+    setIsChecked(newValue);
+    console.log("New value:", newValue);
+    if (onChange) {
+      onChange(newValue);
+    }
   };
 
   return (
@@ -23,14 +27,6 @@ function Checkbox({ label, onChange }) {
       {label}
     </label>
   );
-
-  //   return (
-  //     <label className="container">
-  //       <input type="checkbox" checked={value} onChange={checkHandler} />
-  //       <span className="checkmark"></span>
-  //       {label}
-  //     </label>
-  //   );
 }
 
 export default Checkbox;
